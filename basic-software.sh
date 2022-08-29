09079557141ed68e6acb8b7a6be1c5edbc9de1d3
Eduardo Ramos <67239361+edramoscm@users.noreply.github.com>
2022-08-29 20:04:01 -0300

    Update basic-software.sh

--- basic-software.sh
+++ basic-software.sh
@@ -2,4 +2,4 @@
 
 sudo pacman -Syyu
 
-yay -S audacity calcurse calibre dbeaver deluge-gtk discord etcher-bin gimp gotktrix inetutils keepassxc mgba-qt nordvpn-bin obs-studio postgresql redshift r rstudio-desktop-bin signal-desktop steam telegram-desktop teams w3m zoom --noconfirm
+yay -S audacity calcurse calibre dbeaver deluge-gtk discord etcher-bin gimp gotktrix inetutils keepassxc mgba-qt nordvpn-bin obs-studio postgresql redshift r rstudio-desktop-bin signal-desktop steam telegram-desktop texlive-most texlive-lang teams w3m zoom --noconfirm
